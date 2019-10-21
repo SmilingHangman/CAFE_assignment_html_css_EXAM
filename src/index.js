7d1755c8f93785f56a2fe1f01d5cf686367295f4
SmilingHangman <andrejus.chomicenka@gmail.com>
2019-10-21 19:27:16 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './assets/style.scss';
-import image from './assets/bnb.png';
-const section = document.createElement('div');
-document.body.appendChild(section);
+// import image from './assets/bnb.png';
+// const section = document.createElement('div');
+// document.body.appendChild(section);
 
-const img = new Image();
-img.src = image;
-document.body.appendChild(img);
+// const img = new Image();
+// img.src = image;
+// document.body.appendChild(img);
 
