8b04a782f69e97f7299cb8fedcaf69715463e97a
SmilingHangman <andrejus.chomicenka@gmail.com>
2019-10-23 18:24:26 +0300

    portfolio assets added

--- src/index.js
+++ src/index.js
@@ -6,3 +6,7 @@ import './assets/images/gears_icon.png';
 import './assets/images/rocket_icon.png';
 import './assets/images/contacts_3.png';
 import './assets/images/contacts_4.png';
+import './assets/images/portfolio_01.png';
+import './assets/images/portfolio_02.png';
+import './assets/images/portfolio_03.png';
+import './assets/images/portfolio_04.png';
