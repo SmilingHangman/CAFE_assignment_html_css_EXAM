f99b761be4d61b35fd8bb1f0789a5951125a9ce4
SmilingHangman <andrejus.chomicenka@gmail.com>
2019-10-23 21:41:31 +0300

    navigation added

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './assets/images/flag_icon.png';
 import './assets/images/crayon_icon.png';
 import './assets/images/gears_icon.png';
 import './assets/images/rocket_icon.png';
-import './assets/images/contacts_3.png';
-import './assets/images/contacts_4.png';
 import './assets/images/portfolio_01.png';
 import './assets/images/portfolio_02.png';
 import './assets/images/portfolio_03.png';
@@ -14,3 +12,17 @@ import './assets/images/fb_icon.png';
 import './assets/images/linkedIn_icon.png';
 import './assets/images/mail_icon.png';
 import './assets/images/twitter_icon.png';
+
+
+// navigation smooth scroll below
+$('a[href*="#"]').on('click', function (e) {
+    e.preventDefault()
+
+    $('html, body').animate(
+        {
+            scrollTop: $($(this).attr('href')).offset().top-133,
+        },
+        700,
+        'linear'
+    )
+});
